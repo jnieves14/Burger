fdc36288884a9520ffda55630d26d0f840cb8286
Jannah June Nieves <jannahjune@gmail.com>
2019-09-27 21:53:08 -0700

    initial files per hw instructions

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Ultimate Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The A.B.C Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Diet Burger', true);
+
